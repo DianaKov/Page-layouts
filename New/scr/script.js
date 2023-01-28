4b464d05b62a62f7a167125165a12410ad65afcc
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-01-28 21:22:13 +0200

    Update script.js

--- New/scr/script.js
+++ New/scr/script.js
@@ -3,56 +3,40 @@ var image = document.getElementById('imagetest');
 var imageArray = ["./img/Image.png", "./img/Images.png", "./img/Image2.png","./img/Image3.png"];
 var kettles = document.getElementById["btn-prev", "btn-prev1", "btn-prev2","btn-prev3"];
 var choice = document.getElementById["сhoice", "сhoice2", "сhoice3", "сhoice4"];
+var color = ['white', '#95CFD5', '#C42129', '#E0ADC5', '#F5C7B3'];
+let screenObj = window.screen.width;
+let nav = document.getElementById("nav");
 
 function showColorBlue() {
   document.body.style.backgroundColor = '#95CFD5';
   imagetest.src = imageArray[0];
   choice[0].checked = true;
   kettles[0].style.backgroundColor='white';
-  kettles[3].style.backgroundColor='#95CFD5';
-  kettles[2].style.backgroundColor='#95CFD5';
-  kettles[1].style.backgroundColor='#95CFD5';
+  kettles[1][2][3].style.backgroundColor='#95CFD5';
 }
 function showColorRed() {
   document.body.style.backgroundColor = '#C42129';
   imagetest.src = imageArray[1];
   choice[1].checked = true;
   kettles[1].style.backgroundColor='white';
-  kettles[0].style.backgroundColor='#C42129';
-  kettles[2].style.backgroundColor='#C42129';
-  kettles[3].style.backgroundColor='#C42129';
+  kettles[0][2][3].style.backgroundColor='#C42129';
 }
 function showColorPink() {
   document.body.style.backgroundColor = '#E0ADC5';
   imagetest.src = imageArray[2];
   choice[2].checked = true;
   kettles[2].style.backgroundColor='white';
-  kettles[1].style.backgroundColor='#E0ADC5';
-  kettles[0].style.backgroundColor='#E0ADC5';
-  kettles[3].style.backgroundColor='#E0ADC5';
+  kettles[0][1][3].style.backgroundColor='#E0ADC5';
 }
 function showColorBeige() {
   document.body.style.backgroundColor = '#F5C7B3';
   imagetest.src = imageArray[3];
   choice[3].checked = true;
   kettles[3].style.backgroundColor='white';
-  kettles[2].style.backgroundColor='#F5C7B3';
-  kettles[1].style.backgroundColor='#F5C7B3';
-  kettles[0].style.backgroundColor='#F5C7B3';
+  kettles[0][1][2].style.backgroundColor='#F5C7B3';
 }
-// при выборе одного из трех пунктов меню, он меня он на черный, цвет текста белый, находиться по средине
-function menu(){
-  let screenObj = window.screen.width;
-  if (screenObj < 769) {
-      document.getElementById("nav").style.backgroundColor='black';
-      document.getElementById("nav").style.height="40px";
-      document.getElementById("txt").style.color='white';
-      document.getElementById("txt1").style.color='white';
-      document.getElementById("txt2").style.color='white';
-    } else {
-      document.getElementById("nav").style.backgroundColor='white';
-  }
- }
+
+//мобильная версия
 
 //делаем кликабельно кнопку меню
 document.getElementById("sandwichmenu").addEventListener('click', (e) => {
@@ -71,31 +55,47 @@ function subMenu(a,b){
 }
 
 subMenu(".more",".box");
-subMenu(".more1",".box1");
+/*subMenu(".more1",".box1");
 subMenu(".more2",".box2");
 subMenu(".more3",".box3");
 subMenu(".more4",".box4");
 subMenu(".more5",".box5");
 subMenu(".more6",".box6");
 subMenu(".more7",".box7");
-subMenu(".more8",".box8");
-  
+subMenu(".more8",".box8");*/
+
+// при выборе одного из трех пунктов меню, он меня он на черный, цвет текста белый, находиться по средине
+function menu(){
+  if (screenObj < 769) {
+    nav.style.backgroundColor='black';
+    nav.style.height="40px";
+      document.getElementById("txt").style.color='white';
+      document.getElementById("txt1").style.color='white';
+      document.getElementById("txt2").style.color='white';
+  } else {
+    nav.style.backgroundColor='white';
+  }
+}
+
 //при выборе одного элемента в блоке меню, остальные скрываються
+var down = document.getElementById("down");
+var down2 = document.getElementById("down2");
+var down3 = document.getElementById("down3");
 
-document.getElementById("down").addEventListener('click', (e) => {
+down.addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementById("down2").style.display="none";
- document.getElementById("down3").style.display="none";
+  down2.style.display="none";
+  down3.style.display="none";
 });
 
-document.getElementById("down2").addEventListener('click', (e) => {
+down2.addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementById("down").style.display="none";
-  document.getElementById("down3").style.display="none";
+  down.style.display="none";
+  down3.style.display="none";
 });
 
-document.getElementById("down3").addEventListener('click', (e) => {
+down3.addEventListener('click', (e) => {
   e.preventDefault();
-  document.getElementById("down").style.display="none";
-  document.getElementById("down2").style.display="none";
+  down.style.display="none";
+  down2.style.display="none";
 });
