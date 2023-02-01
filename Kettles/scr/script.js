71193f13d892e3ca7eae2cb59954c49ea8ac8821
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-01 20:35:03 +0200

    Update script.js

--- Kettles/scr/script.js
+++ Kettles/scr/script.js
@@ -13,6 +13,7 @@ function showColorRed() {
   document.body.style.backgroundColor = '#C42129';
   imagetest.src = imageArray[1];
   document.getElementById("сhoice2").checked = true;
+
 }
 function showColorPink() {
   document.body.style.backgroundColor = '#E0ADC5';
@@ -25,65 +26,51 @@ function showColorBeige() {
   document.getElementById("сhoice4").checked = true;
 }
 
-function showColorWhite(a){
-  document.querySelector(a).addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector(a).style.backgroundColor='white';;
-  });
-}
-showColorWhite("сhoice");
-showColorWhite("сhoice2");
-showColorWhite("сhoice3");
-showColorWhite("сhoice4");
 //мобильная версия
+var down = document.getElementById("down");
+var down2 = document.getElementById("down2");
+var down3 = document.getElementById("down3");
 
 //делаем кликабельно кнопку меню
   document.getElementById("sandwichmenu").addEventListener('click', (e) => {
       e.preventDefault();
       document.getElementById("sandwichmenu").classList.toggle("active");
-      document.querySelector(".nav").classList.toggle("active");
-      document.getElementById("sandwichmenu").classList.toggle("active");
-      
-      
-
-      //внутренний выпадающий список
-
-    function subMenu(a,b){
-      document.querySelector(a).addEventListener('click', (e) => {
-        e.preventDefault();
-        document.querySelector(a).classList.toggle("active");
-        document.querySelector(b).classList.toggle("active");
-      });
-    }
-
-    subMenu(".more",".box");
-    subMenu(".more1",".box1");
-    subMenu(".more2",".box2");
-    subMenu(".more3",".box3");
-    subMenu(".more4",".box4");
-    subMenu(".more5",".box5");
-    subMenu(".more6",".box6");
-    subMenu(".more7",".box7");
-    subMenu(".more8",".box8");
+      document.querySelector(".nav").classList.toggle("active");      
   }); 
 
+  //при выборе одного элемента в блоке меню, остальные скрываються
 
-//при выборе одного элемента в блоке меню, остальные скрываються + цвет фона меняем на черный, а текста на белый
-var down = document.getElementById("down");
-var down2 = document.getElementById("down2");
-var down3 = document.getElementById("down3");
+  function sample (a,b,c){
+    a.addEventListener('click', (e) => {
+      e.preventDefault();
+        b.style.display="none";
+        c.style.display="none";
+    })
+  };
 
-function sample (a,b,c){
-  a.addEventListener('click', (e) => {
-    e.preventDefault();
-    b.style.display="none";
-    c.style.display="none";
-  });
-}
+  sample(down,down2,down3);
+  sample(down2,down,down3);
+  sample(down3,down,down2);
 
-sample(down,down2,down3);
-sample(down2,down,down3);
-sample(down3,down,down2);
+
+  //внутренний выпадающий список
+  function subMenu(a,b){
+    document.querySelector(a).addEventListener('click', (e) => {
+      e.preventDefault();
+      document.querySelector(a).classList.toggle("active");
+      document.querySelector(b).classList.toggle("active");
+    });
+  }
+
+  subMenu(".more",".box");
+  subMenu(".more1",".box1");
+  subMenu(".more2",".box2");
+  subMenu(".more3",".box3");
+  subMenu(".more4",".box4");
+  subMenu(".more5",".box5");
+  subMenu(".more6",".box6");
+  subMenu(".more7",".box7");
+  subMenu(".more8",".box8");
 
 // модальнык окна
 var showModal = document.querySelector('.showModal');
