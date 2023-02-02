2798a12628af06f63470d7d984b80115dabe214a
Diana Kovalevska <119618395+DianaKov@users.noreply.github.com>
2023-02-02 19:45:24 +0200

    Update script.js

--- Kettles/scr/script.js
+++ Kettles/scr/script.js
@@ -3,27 +3,41 @@ var image = document.getElementById('imagetest');
 var imageArray = ["./img/Image.png", "./img/Images.png", "./img/Image2.png","./img/Image3.png"];
 let screenObj = window.screen.width;
 let nav = document.getElementById("nav");
+var btn1 = document.getElementById("btn-prev");
+var btn2 = document.getElementById("btn-prev1");
+var btn3 = document.getElementById("btn-prev2");
+var btn4 = document.getElementById("btn-prev3");
+
+function showColorWhite(a,b,c,d){
+  btn1.style.backgroundColor=a;
+  btn2.style.backgroundColor=b;
+  btn3.style.backgroundColor=c;
+  btn4.style.backgroundColor=d;
+}
 
 function showColorBlue() {
   document.body.style.backgroundColor = '#95CFD5';
   imagetest.src = imageArray[0];
   document.getElementById("сhoice").checked = true;
+  showColorWhite('white','#95CFD5','#95CFD5','#95CFD5');
 }
 function showColorRed() {
   document.body.style.backgroundColor = '#C42129';
   imagetest.src = imageArray[1];
   document.getElementById("сhoice2").checked = true;
-
+  showColorWhite('#C42129','white','#C42129','#C42129');
 }
 function showColorPink() {
   document.body.style.backgroundColor = '#E0ADC5';
   imagetest.src = imageArray[2];
   document.getElementById("сhoice3").checked = true;
+  showColorWhite('#E0ADC5','#E0ADC5','white','#E0ADC5');
 }
 function showColorBeige() {
   document.body.style.backgroundColor = '#F5C7B3';
   imagetest.src = imageArray[3];
   document.getElementById("сhoice4").checked = true;
+  showColorWhite('#F5C7B3','#F5C7B3','#F5C7B3','white');
 }
 
 //мобильная версия
@@ -32,15 +46,19 @@ var down2 = document.getElementById("down2");
 var down3 = document.getElementById("down3");
 
 //делаем кликабельно кнопку меню
-  document.getElementById("sandwichmenu").addEventListener('click', (e) => {
-      e.preventDefault();
-      document.getElementById("sandwichmenu").classList.toggle("active");
-      document.querySelector(".nav").classList.toggle("active");      
-  }); 
+  
 
   //при выборе одного элемента в блоке меню, остальные скрываються
 
   function sample (a,b,c){
+    document.getElementById("sandwichmenu").addEventListener('click', (e) => {
+      e.preventDefault();
+      document.getElementById("sandwichmenu").classList.toggle("active");
+      document.querySelector(".nav").classList.toggle("active");  
+      b.style.display="block";
+      c.style.display="block";
+    }); 
+
     a.addEventListener('click', (e) => {
       e.preventDefault();
         b.style.display="none";
